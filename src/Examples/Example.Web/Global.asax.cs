5cb6032593d32949b07e721c1dde33ef26685e8f
Chad Lee <git@chadly.net>
2009-06-09 21:03:51 +0000

    Adding example mvc project that uses IOC with the geocoding api.
    git-svn-id: http://geocoding-net.googlecode.com/svn/trunk@30 928293fd-5c4c-0410-ab33-177afc521ce9

---
+++ src/Examples/Example.Web/Global.asax.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Autofac.Builder;
+using Autofac.Integration.Web;
+using Autofac.Integration.Web.Mvc;
+using GeoCoding.Services;
+using GeoCoding.Services.Google;
+using GeoCoding.Services.VirtualEarth;
+using GeoCoding.Services.Yahoo;
+
+namespace Example.Web
+{
+	public class MvcApplication : System.Web.HttpApplication, IContainerProviderAccessor
+	{
+		private static IContainerProvider containerProvider;
+
+		public IContainerProvider ContainerProvider
+		{
+			get { return containerProvider; }
+		}
+
+		private void InitializeContainer()
+		{
+			var builder = new ContainerBuilder();
+			builder.RegisterModule(new AutofacControllerModule(Assembly.GetExecutingAssembly()));
+
+			//register your geocoder implementation here -- note: it will use the last one registered
+			builder.Register(c => new VirtualEarthGeoCoder("my-virtual-earth-username", "my-virtual-earth-password")).As<IGeoCoder>();
+			builder.Register(c => new YahooGeoCoder("my-yahoo-app-id")).As<IGeoCoder>();
+			builder.Register(c => new GoogleGeoCoder("my-google-api-key")).As<IGeoCoder>();
+
+			containerProvider = new ContainerProvider(builder.Build());
+			ControllerBuilder.Current.SetControllerFactory(new AutofacControllerFactory(ContainerProvider));
+		}
+
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute(
+				"Default",                                              // Route name
+				"{controller}/{action}/{id}",                           // URL with parameters
+				new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
+			);
+		}
+
+		protected void Application_Start()
+		{
+			InitializeContainer();
+			RegisterRoutes(RouteTable.Routes);
+		}
+	}
+}
