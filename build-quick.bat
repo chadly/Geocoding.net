563fe74348bfe5264949baffe51c66e3c64dc785
Chad Lee <git@chadly.net>
2011-11-02 17:24:14 +0000

    Updating build script to merge outputted assemblies.
    git-svn-id: http://geocoding-net.googlecode.com/svn/trunk@52 928293fd-5c4c-0410-ab33-177afc521ce9

--- build-quick.bat
+++ build-quick.bat
@@ -1,3 +1,3 @@
 @echo off
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe geocoding.build /t:CopyTemplateFiles;Version;Compile /nologo
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe geocoding.build /t:Compile /nologo
 pause
