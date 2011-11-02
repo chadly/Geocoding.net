adc119691fbf0019a1a20b7ce99bb443bd816a40
Chad Lee <git@chadly.net>
2011-11-02 16:50:09 +0000

    Updating MsBuild community tasks, including ILMerge, and updating refactoring build script.
    git-svn-id: http://geocoding-net.googlecode.com/svn/trunk@51 928293fd-5c4c-0410-ab33-177afc521ce9

--- build-quick.bat
+++ build-quick.bat
@@ -1,3 +1,3 @@
 @echo off
-%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe src\geocoding.build /t:Version;Compile /nologo
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe geocoding.build /t:CopyTemplateFiles;Version;Compile /nologo
 pause
--- build-release.bat
+++ build-release.bat
@@ -1,3 +1,3 @@
 @echo off
-%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe src\geocoding.build /p:Configuration=Release /nologo
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe geocoding.build /p:Configuration=Release /nologo
 pause
