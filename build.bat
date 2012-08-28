57d00bfb4946add24cfa92dadc4f600705b66565
Chad Lee <git@chadly.net>
2012-08-27 20:53:30 -0500

    Moving files around

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe geocoding.build /t:Compile /nologo
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe src\geocoding.build /t:Compile /nologo
 pause
--- clean.bat
+++ clean.bat
@@ -1,3 +1,3 @@
 @echo off
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe geocoding.build /t:Clean /nologo
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe src\geocoding.build /t:Clean /nologo
 pause
