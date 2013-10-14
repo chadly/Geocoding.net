198da8dfaaa99d34ff97fba2e8bcc724751cc8cf
Dreiundzwanzig <brom.wow@gmail.com>
2013-10-14 10:23:38 +0200

    Added features to Bing Maps geocoder
    Added features to Bing Map geocoder (web proxy, culture, user location,
    user map view, user IP)
    Added tests for those features
    
    Credits: https://github.com/manuel-guilbault/Geocoding.net/commit/1205efa22824fd716d5350e80c7920b9ed6a9cd4
    
    Signed-off-by: Dreiundzwanzig <brom.wow@gmail.com>

--- src/Tests/BingMapsTest.cs
+++ src/Tests/BingMapsTest.cs
@@ -1,14 +1,54 @@
 ﻿using System;
+using System.Linq;
 using System.Configuration;
 using GeoCoding.Microsoft;
+using Xunit.Extensions;
+using Xunit;
+using System.Net;
 
 namespace GeoCoding.Tests
 {
 	public class BingMapsTest : GeoCoderTest
 	{
+		BingMapsGeoCoder geoCoder;
+
 		protected override IGeoCoder CreateGeoCoder()
 		{
-			return new BingMapsGeoCoder(ConfigurationManager.AppSettings["bingMapsKey"]);
+			geoCoder = new BingMapsGeoCoder(ConfigurationManager.AppSettings["bingMapsKey"]);
+			return geoCoder;
+		}
+
+		[Theory]
+		[InlineData("United States", "fr", "États-Unis")]
+		[InlineData("Montreal", "en", "Montreal, QC")]
+		[InlineData("Montreal", "fr", "Montréal, QC")]
+		public void ApplyCulture(string address, string culture, string result)
+		{
+			geoCoder.Culture = culture;
+			BingAddress[] addresses = geoCoder.GeoCode(address).ToArray();
+			Assert.Equal(result, addresses[0].FormattedAddress);
+		}
+
+		[Theory]
+		[InlineData("Montreal", 45.512401580810547, -73.554679870605469, "Canada")]
+		[InlineData("Montreal", 43.949058532714844, 0.20011000335216522, "France")]
+		[InlineData("Montreal", 46.428329467773438, -90.241783142089844, "United States")]
+		public void ApplyUserLocation(string address, double userLatitude, double userLongitude, string country)
+		{
+			geoCoder.UserLocation = new Location(userLatitude, userLongitude);
+			BingAddress[] addresses = geoCoder.GeoCode(address).ToArray();
+			Assert.Equal(country, addresses[0].CountryRegion);
+		}
+
+		[Theory]
+		[InlineData("Montreal", 45, -73, 46, -74, "Canada")]
+		[InlineData("Montreal", 43, 0, 44, 1, "France")]
+		[InlineData("Montreal", 46, -90, 47, -91, "United States")]
+		public void ApplyUserMapView(string address, double userLatitude1, double userLongitude1, double userLatitude2, double userLongitude2, string country)
+		{
+			geoCoder.UserMapView = new Bounds(userLatitude1, userLongitude1, userLatitude2, userLongitude2);
+			BingAddress[] addresses = geoCoder.GeoCode(address).ToArray();
+			Assert.Equal(country, addresses[0].CountryRegion);
 		}
 	}
 }
