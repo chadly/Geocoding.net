c2c3e6bec29a314f50b10879938b6ef81164ff17
Dreiundzwanzig <brom.wow@gmail.com>
2013-10-14 09:24:03 +0200

    Async support & Google Maps new options
    Support for asynchronous calls.
    New options for Google Maps geocoder (HTTP proxy, language, biasing).
    
    Including Tabfix
    Credits: https://github.com/manuel-guilbault/Geocoding.net/commit/52002e7f2977ea7e98a005ac0aefbb488a7abb8d
    
    Signed-off-by: Dreiundzwanzig <brom.wow@gmail.com>

---
+++ src/Core/IAsyncGeoCoder.cs
@@ -0,0 +1,18 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace GeoCoding
+{
+    public interface IAsyncGeoCoder
+    {
+        Task<IEnumerable<Address>> GeoCodeAsync(string address);
+        Task<IEnumerable<Address>> GeoCodeAsync(string address, CancellationToken cancellationToken);
+        Task<IEnumerable<Address>> GeoCodeAsync(string street, string city, string state, string postalCode, string country);
+        Task<IEnumerable<Address>> GeoCodeAsync(string street, string city, string state, string postalCode, string country, CancellationToken cancellationToken);
+
+        Task<IEnumerable<Address>> ReverseGeocodeAsync(double latitude, double longitude);
+        Task<IEnumerable<Address>> ReverseGeocodeAsync(double latitude, double longitude, CancellationToken cancellationToken);
+    }
+}
