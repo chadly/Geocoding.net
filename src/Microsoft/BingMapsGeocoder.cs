1db7356a26bb4c61848c8d8321bf6529686190c1
Dreiundzwanzig <brom.wow@gmail.com>
2013-10-14 09:55:58 +0200

    Async implementation of Bing Maps
    Credits: https://github.com/manuel-guilbault/Geocoding.net/commit/b53500a78cdaae0f92209f26dd4becac3a3e129d
    
    Signed-off-by: Dreiundzwanzig <brom.wow@gmail.com>

--- src/Microsoft/BingMapsGeocoder.cs
+++ src/Microsoft/BingMapsGeocoder.cs
@@ -6,38 +6,65 @@ using System.Net;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Web;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace GeoCoding.Microsoft
 {
 	/// <remarks>
 	/// http://msdn.microsoft.com/en-us/library/ff701715.aspx
 	/// </remarks>
-	public class BingMapsGeoCoder : IGeoCoder
+	public class BingMapsGeoCoder : IGeoCoder, IAsyncGeoCoder
 	{
-		const string UNFORMATTED_QUERY = "http://dev.virtualearth.net/REST/v1/Locations/{0}?key={1}";
-		const string FORMATTED_QUERY = "http://dev.virtualearth.net/REST/v1/Locations?{0}&key={1}";
-		const string QUERY = "q={0}";
-		const string COUNTRY = "countryRegion={0}";
-		const string ADMIN = "adminDistrict={0}";
-		const string ZIP = "postalCode={0}";
-		const string CITY = "locality={0}";
-		const string ADDRESS = "addressLine={0}";
+		private const string UNFORMATTED_QUERY = "http://dev.virtualearth.net/REST/v1/Locations/{0}?key={1}";
+		private const string FORMATTED_QUERY = "http://dev.virtualearth.net/REST/v1/Locations?{0}&key={1}";
+		private const string QUERY = "q={0}";
+		private const string COUNTRY = "countryRegion={0}";
+		private const string ADMIN = "adminDistrict={0}";
+		private const string ZIP = "postalCode={0}";
+		private const string CITY = "locality={0}";
+		private const string ADDRESS = "addressLine={0}";
 
-		readonly string bingKey;
+		private readonly string bingKey;
 
 		public BingMapsGeoCoder(string bingKey)
 		{
 			this.bingKey = bingKey;
 		}
 
+		private string GetQueryUrl(string address)
+		{
+			var parameters = new StringBuilder();
+			AppendParameter(parameters, address, QUERY, true);
+
+			return string.Format(FORMATTED_QUERY, parameters.ToString(), bingKey);
+		}
+
+		private string GetQueryUrl(string street, string city, string state, string postalCode, string country)
+		{
+			StringBuilder parameters = new StringBuilder();
+			bool first = true;
+			first = AppendParameter(parameters, city, CITY, first);
+			first = AppendParameter(parameters, state, ADMIN, first);
+			first = AppendParameter(parameters, postalCode, ZIP, first);
+			first = AppendParameter(parameters, country, COUNTRY, first);
+			first = AppendParameter(parameters, street, ADDRESS, first);
+
+			return string.Format(FORMATTED_QUERY, parameters.ToString(), bingKey);
+		}
+
+		private string GetQueryUrl(double latitude, double longitude)
+		{
+			return string.Format(UNFORMATTED_QUERY,
+				String.Format(CultureInfo.InvariantCulture, "{0},{1}", latitude, longitude), bingKey);
+		}
+
 		public IEnumerable<BingAddress> GeoCode(string address)
 		{
 			try
 			{
-				var parameters = new StringBuilder();
-				AppendParameter(parameters, address, QUERY, true);
-
-				var response = GetResponse(string.Format(FORMATTED_QUERY, parameters.ToString(), bingKey));
+				var url = GetQueryUrl(address);
+				var response = GetResponse(url);
 				return ParseResponse(response);
 			}
 			catch (Exception ex)
@@ -46,23 +73,13 @@ namespace GeoCoding.Microsoft
 			}
 		}
 
-		public IEnumerable<BingAddress> GeoCode(string street, string city, string state, string postalCode, string country)
+		public IEnumerable<BingAddress> GeoCode(string street, string city, string state, string postalCode,
+			string country)
 		{
 			try
 			{
-				StringBuilder parameters = new StringBuilder();
-				bool first = true;
-				first = AppendParameter(parameters, city, CITY, first);
-				first = AppendParameter(parameters, state, ADMIN, first);
-				first = AppendParameter(parameters, postalCode, ZIP, first);
-				first = AppendParameter(parameters, country, COUNTRY, first);
-				first = AppendParameter(parameters, street, ADDRESS, first);
-
-				var response = GetResponse(
-					string.Format(
-						FORMATTED_QUERY,
-						parameters.ToString(),
-						bingKey));
+				var url = GetQueryUrl(street, city, state, postalCode, country);
+				var response = GetResponse(url);
 				return ParseResponse(response);
 			}
 			catch (Exception ex)
@@ -83,7 +100,8 @@ namespace GeoCoding.Microsoft
 		{
 			try
 			{
-				var response = GetResponse(string.Format(UNFORMATTED_QUERY, String.Format(CultureInfo.InvariantCulture, "{0},{1}", latitude, longitude), bingKey));
+				var url = GetQueryUrl(latitude, longitude);
+				var response = GetResponse(url);
 				return ParseResponse(response);
 			}
 			catch (Exception ex)
@@ -92,12 +110,58 @@ namespace GeoCoding.Microsoft
 			}
 		}
 
+		public Task<IEnumerable<BingAddress>> GeoCodeAsync(string address)
+		{
+			var url = GetQueryUrl(address);
+			return GetResponseAsync(url)
+				.ContinueWith(task => ParseResponse(task.Result));
+		}
+
+		public Task<IEnumerable<BingAddress>> GeoCodeAsync(string address, CancellationToken cancellationToken)
+		{
+			var url = GetQueryUrl(address);
+			return GetResponseAsync(url, cancellationToken)
+				.ContinueWith(task => ParseResponse(task.Result), cancellationToken);
+		}
+
+		public Task<IEnumerable<BingAddress>> GeoCodeAsync(string street, string city, string state, string postalCode,
+			string country)
+		{
+			var url = GetQueryUrl(street, city, state, postalCode, country);
+			return GetResponseAsync(url)
+				.ContinueWith(task => ParseResponse(task.Result));
+		}
+
+		public Task<IEnumerable<BingAddress>> GeoCodeAsync(string street, string city, string state, string postalCode,
+			string country, CancellationToken cancellationToken)
+		{
+			var url = GetQueryUrl(street, city, state, postalCode, country);
+			return GetResponseAsync(url, cancellationToken)
+				.ContinueWith(task => ParseResponse(task.Result), cancellationToken);
+		}
+
+		public Task<IEnumerable<BingAddress>> ReverseGeocodeAsync(double latitude, double longitude)
+		{
+			var url = GetQueryUrl(latitude, longitude);
+			return GetResponseAsync(url)
+				.ContinueWith(task => ParseResponse(task.Result));
+		}
+
+		public Task<IEnumerable<BingAddress>> ReverseGeocodeAsync(double latitude, double longitude,
+			CancellationToken cancellationToken)
+		{
+			var url = GetQueryUrl(latitude, longitude);
+			return GetResponseAsync(url, cancellationToken)
+				.ContinueWith(task => ParseResponse(task.Result), cancellationToken);
+		}
+
 		IEnumerable<Address> IGeoCoder.GeoCode(string address)
 		{
 			return GeoCode(address).Cast<Address>();
 		}
 
-		IEnumerable<Address> IGeoCoder.GeoCode(string street, string city, string state, string postalCode, string country)
+		IEnumerable<Address> IGeoCoder.GeoCode(string street, string city, string state, string postalCode,
+			string country)
 		{
 			return GeoCode(street, city, state, postalCode, country).Cast<Address>();
 		}
@@ -110,6 +174,45 @@ namespace GeoCoding.Microsoft
 		IEnumerable<Address> IGeoCoder.ReverseGeocode(double latitude, double longitude)
 		{
 			return ReverseGeoCode(latitude, longitude).Cast<Address>();
+		}
+
+		Task<IEnumerable<Address>> IAsyncGeoCoder.GeoCodeAsync(string address)
+		{
+			return GeoCodeAsync(address)
+				.ContinueWith(task => task.Result.Cast<Address>());
+		}
+
+		Task<IEnumerable<Address>> IAsyncGeoCoder.GeoCodeAsync(string address, CancellationToken cancellationToken)
+		{
+			return GeoCodeAsync(address, cancellationToken)
+				.ContinueWith(task => task.Result.Cast<Address>(), cancellationToken);
+		}
+
+		Task<IEnumerable<Address>> IAsyncGeoCoder.GeoCodeAsync(string street, string city, string state,
+			string postalCode, string country)
+		{
+			return GeoCodeAsync(street, city, state, postalCode, country)
+				.ContinueWith(task => task.Result.Cast<Address>());
+		}
+
+		Task<IEnumerable<Address>> IAsyncGeoCoder.GeoCodeAsync(string street, string city, string state,
+			string postalCode, string country, CancellationToken cancellationToken)
+		{
+			return GeoCodeAsync(street, city, state, postalCode, country, cancellationToken)
+				.ContinueWith(task => task.Result.Cast<Address>(), cancellationToken);
+		}
+
+		Task<IEnumerable<Address>> IAsyncGeoCoder.ReverseGeocodeAsync(double latitude, double longitude)
+		{
+			return ReverseGeocodeAsync(latitude, longitude)
+				.ContinueWith(task => task.Result.Cast<Address>());
+		}
+
+		Task<IEnumerable<Address>> IAsyncGeoCoder.ReverseGeocodeAsync(double latitude, double longitude,
+			CancellationToken cancellationToken)
+		{
+			return ReverseGeocodeAsync(latitude, longitude, cancellationToken)
+				.ContinueWith(task => task.Result.Cast<Address>(), cancellationToken);
 		}
 
 		private bool AppendParameter(StringBuilder sb, string parameter, string format, bool first)
@@ -139,9 +242,9 @@ namespace GeoCoding.Microsoft
 					location.Address.CountryRegion,
 					location.Address.Locality,
 					location.Address.PostalCode,
-					(EntityType)Enum.Parse(typeof(EntityType), location.EntityType),
+					(EntityType) Enum.Parse(typeof (EntityType), location.EntityType),
 					EvaluateConfidence(location.Confidence)
-				);
+					);
 			}
 		}
 
@@ -150,8 +253,63 @@ namespace GeoCoding.Microsoft
 			HttpWebRequest request = WebRequest.Create(queryURL) as HttpWebRequest;
 			using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
 			{
-				DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(Json.Response));
+				DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof (Json.Response));
 				return jsonSerializer.ReadObject(response.GetResponseStream()) as Json.Response;
+			}
+		}
+
+		private Task<Json.Response> GetResponseAsync(string queryURL, CancellationToken? cancellationToken = null)
+		{
+			HttpWebRequest request = (HttpWebRequest) WebRequest.Create(queryURL);
+
+			if (cancellationToken != null)
+			{
+				cancellationToken.Value.ThrowIfCancellationRequested();
+				cancellationToken.Value.Register(() => request.Abort());
+			}
+
+			var requestState = new RequestState(request, cancellationToken);
+			return Task.Factory.FromAsync(
+				(callback, asyncState) => SendRequestAsync((RequestState) asyncState, callback),
+				result => ProcessResponseAsync((RequestState) result.AsyncState, result),
+				requestState
+				);
+		}
+
+		private IAsyncResult SendRequestAsync(RequestState requestState, AsyncCallback callback)
+		{
+			try
+			{
+				return requestState.request.BeginGetResponse(callback, requestState);
+			}
+			catch (Exception ex)
+			{
+				throw new BingGeoCodingException(ex);
+			}
+		}
+
+		private Json.Response ProcessResponseAsync(RequestState requestState, IAsyncResult result)
+		{
+			if (requestState.cancellationToken != null)
+				requestState.cancellationToken.Value.ThrowIfCancellationRequested();
+
+			try
+			{
+				using (var response = (HttpWebResponse) requestState.request.EndGetResponse(result))
+				{
+					DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof (Json.Response));
+					return jsonSerializer.ReadObject(response.GetResponseStream()) as Json.Response;
+				}
+			}
+			catch (BingGeoCodingException)
+			{
+				//let these pass through
+				throw;
+			}
+			catch (Exception ex)
+			{
+				//wrap in google exception
+				throw new BingGeoCodingException(ex);
 			}
 		}
 
@@ -176,6 +334,20 @@ namespace GeoCoding.Microsoft
 				return string.Empty;
 
 			return HttpUtility.UrlEncode(toEncode);
+		}
+
+		protected class RequestState
+		{
+			public readonly HttpWebRequest request;
+			public readonly CancellationToken? cancellationToken;
+
+			public RequestState(HttpWebRequest request, CancellationToken? cancellationToken)
+			{
+				if (request == null) throw new ArgumentNullException("request");
+
+				this.request = request;
+				this.cancellationToken = cancellationToken;
+			}
 		}
 	}
 }
