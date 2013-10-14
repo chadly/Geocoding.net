69ada990434be1c5e56856f95f1261a49a1bf612
Dreiundzwanzig <brom.wow@gmail.com>
2013-10-14 10:13:14 +0200

    Added tests for async Bing Maps
    Credits: https://github.com/manuel-guilbault/Geocoding.net/commit/13ab03f8f8067b730f80859ad830e6dd672fcd95
    
    Signed-off-by: Dreiundzwanzig <brom.wow@gmail.com>

---
+++ src/Tests/BingMapsAsyncTest.cs
@@ -0,0 +1,37 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeoCoding.Microsoft;
+using System.Configuration;
+using Xunit.Extensions;
+using Xunit;
+
+namespace GeoCoding.Tests
+{
+    public class BingMapsAsyncTest : AsyncGeoCoderTest
+    {
+        BingMapsGeoCoder geoCoder;
+
+        protected override IAsyncGeoCoder CreateAsyncGeoCoder()
+        {
+            geoCoder = new BingMapsGeoCoder(ConfigurationManager.AppSettings["bingMapsKey"]);
+            return geoCoder;
+        }
+
+        [Theory]
+        [InlineData("United States", EntityType.CountryRegion)]
+        [InlineData("Illinois, US", EntityType.AdminDivision1)]
+        [InlineData("New York, New York", EntityType.PopulatedPlace)]
+        [InlineData("90210, US", EntityType.Postcode1)]
+        [InlineData("1600 pennsylvania ave washington dc", EntityType.Address)]
+        public void CanParseAddressTypes(string address, EntityType type)
+        {
+            geoCoder.GeoCodeAsync(address).ContinueWith(task =>
+            {
+                BingAddress[] addresses = task.Result.ToArray();
+                Assert.Equal(type, addresses[0].Type);
+            });
+        }
+    }
+}
