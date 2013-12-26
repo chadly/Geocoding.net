5f7b863d821684b924524ed64afc9a3104c30510
Chad Lee <git@chadly.net>
2013-12-26 16:13:33 -0600

    Adding pack.bat to pack for nuget.

---
+++ pack.bat
@@ -0,0 +1,4 @@
+@echo off
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe src\nuget.build /nologo
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe src\geocoding.build /t:Pack /p:Configuration=Release /nologo
+pause
