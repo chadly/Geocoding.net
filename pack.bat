1136549142e41326c136d48989982210e8318e0c
Chad Lee <git@chadly.net>
2014-02-22 11:36:14 -0600

    Reworking build files to be more modular

--- pack.bat
+++ pack.bat
@@ -1,4 +1,3 @@
 @echo off
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe src\nuget.build /nologo
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe src\geocoding.build /t:Pack /p:Configuration=Release /nologo
 pause
