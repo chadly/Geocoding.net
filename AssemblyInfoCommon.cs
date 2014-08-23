05a3127205438b5a3d4ef73afbc19b5ec913b363
Chad Lee <git@chadly.net>
2014-08-23 16:29:52 -0500

    Need AssemblyInfoCommon in there

---
+++ AssemblyInfoCommon.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Geocoding.Net")]
+[assembly: AssemblyCompany("Geocoding.Net")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2014 Chad Lee")]
+[assembly: ComVisible(false)]
