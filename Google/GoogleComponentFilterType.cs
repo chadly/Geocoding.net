330cbabe35a901a1140b106e65f9e28363cc0ac6
Chad Lee <git@chadly.net>
2017-06-13 16:01:02 -0500

    Remove superfluous files

--- Google/GoogleComponentFilterType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Geocoding.Google
-{
-	public struct GoogleComponentFilterType
-	{
-		public const string AdministrativeArea = "administrative_area";
-		public const string PostalCode = "postal_code";
-		public const string Country = "country";
-	}
-}
