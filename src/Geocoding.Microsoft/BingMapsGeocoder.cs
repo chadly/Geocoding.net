d7e1df76a3011b80db5d4d94ba74ebb6d52762f2
Rikki Calteaux <rikkicalteaux+@gmail.com>
2017-09-18 10:33:59 +1000

    Resolved DataContractJsonSerializer

--- src/Geocoding.Microsoft/BingMapsGeocoder.cs
+++ src/Geocoding.Microsoft/BingMapsGeocoder.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 
