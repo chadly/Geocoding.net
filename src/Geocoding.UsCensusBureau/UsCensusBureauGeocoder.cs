e131fc9214f9ac9edc85f626a2817381bc983471
Daniel P. Rossi <DR9885@gmail.com>
2018-12-22 04:49:46 -0800

    first pass with tests

---
+++ src/Geocoding.UsCensusBureau/UsCensusBureauGeocoder.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Geocoding.UsCensusBureau
+{
+	/// <summary>
+	/// refs:
+	/// - https://geocoding.geo.census.gov/
+	/// - https://geocoding.geo.census.gov/geocoder/Geocoding_Services_API.pdf
+	/// </summary>
+	public class UsCensusBureauGeocoder : IGeocoder
+    {
+        private readonly int _benchmark;
+	    private readonly string _format;
+	    private readonly HttpClient _client;
+
+        public UsCensusBureauGeocoder(int benchmark = 4, string format = "json")
+        {
+            _benchmark = benchmark;
+	        _format = format;
+	        _client = new HttpClient { BaseAddress = new Uri(UsCensusBureauConstants.BaseUrl) };
+        }
+        
+        public async Task<IEnumerable<Address>> GeocodeAsync(string address, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Build Query String
+			var sb = new StringBuilder(UsCensusBureauConstants.OneLineAddressPath);
+			sb.Append("address=").Append(WebUtility.UrlEncode(address))
+				.Append("&benchmark=").Append(_benchmark)
+				.Append("&format=").Append(_format);
+            
+            // Get Request
+            var response = await _client.GetAsync(sb.ToString(), cancellationToken);
+	        var content = await response.Content.ReadAsStringAsync();
+            
+            // Read Result
+            return GetAddresses(content);
+        }
+
+        public async  Task<IEnumerable<Address>> GeocodeAsync(string street, string city, string state, string postalCode, string country, CancellationToken cancellationToken = default(CancellationToken))
+        {
+			// Build Query String
+			var sb = new StringBuilder(UsCensusBureauConstants.AddressPath);
+			sb.Append("street=").Append(WebUtility.UrlEncode(street))
+				.Append("&city=").Append(WebUtility.UrlEncode(city))
+				.Append("&state=").Append(WebUtility.UrlEncode(state))
+				.Append("&zip=").Append(WebUtility.UrlEncode(postalCode))
+				.Append("&benchmark=").Append(_benchmark)
+				.Append("&format=").Append(_format);
+            
+	        // Get Request
+	        var response = await _client.GetAsync(sb.ToString(), cancellationToken);
+	        var content = await response.Content.ReadAsStringAsync();
+            
+	        // Read Result
+	        return GetAddresses(content);
+        }
+
+        public Task<IEnumerable<Address>> ReverseGeocodeAsync(Location location, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotSupportedException();
+        }
+
+        public Task<IEnumerable<Address>> ReverseGeocodeAsync(double latitude, double longitude, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotSupportedException();
+        }
+
+        private static IEnumerable<UsCensusBureauAddress> GetAddresses(string response)
+        {
+	        var json = JObject.Parse(response);
+	        
+	        var errors = json[UsCensusBureauConstants.ErrorsKey];
+	        if (errors != null)
+		        return new UsCensusBureauAddress[] {};
+	        
+            var result = json[UsCensusBureauConstants.ResultKey];
+            return result[UsCensusBureauConstants.AddressMatchesKey]
+                .Select(match =>
+                {
+                    var formatted = match[UsCensusBureauConstants.MatchedAddressKey].ToString();
+                    var coordinates = match[UsCensusBureauConstants.CoordinatesKey];
+                    var x = double.Parse(coordinates[UsCensusBureauConstants.XKey].ToString());
+                    var y = double.Parse(coordinates[UsCensusBureauConstants.YKey].ToString());
+                    
+                    return new UsCensusBureauAddress(formatted, new Location(y, x));
+                })
+                .ToArray();
+        }
+    }
+}
---
+++ test/Geocoding.Tests/UsCensusBureauTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Geocoding.UsCensusBureau;
+using Xunit;
+
+namespace Geocoding.Tests
+{
+	[Collection("Settings")]
+	public class UsCensusBureauTest : GeocoderTest
+	{
+		public UsCensusBureauTest(SettingsFixture settings) : base(settings)
+		{
+		}
+
+		protected override IGeocoder CreateGeocoder()
+		{
+			return new UsCensusBureauGeocoder();
+		}
+
+		[Theory(Skip = "not supported - us addresses only")]
+		public override Task CanGeocodeAddressUnderDifferentCultures(string cultureName)
+		{
+			return Task.CompletedTask;
+		}
+
+		[Theory(Skip = "not supported - reverse geocode")]
+		public override Task CanReverseGeocodeAddressUnderDifferentCultures(string cultureName)
+		{
+			return Task.CompletedTask;
+		}
+
+		[Theory(Skip = "using different input with CanGeocodeWithSpecialCharacters2")]
+		public override Task CanGeocodeWithSpecialCharacters(string address)
+		{
+			return Task.CompletedTask;
+		}
+		
+		[Theory]
+		[InlineData("12110 CLAYTON ROAD TOWN & COUNTRY,SAINT LOUIS,MO,63131,US")]
+		public async Task CanGeocodeWithSpecialCharacters2(string address)
+		{
+			Address[] addresses = (await geocoder.GeocodeAsync(address)).ToArray();
+
+			//asserting no exceptions are thrown and that we get something
+			Assert.NotEmpty(addresses);
+		}
+
+		[Theory(Skip = "not supported - exact addresses only")]
+		public override Task CanHandleStreetIntersectionsByAmpersand(string address)
+		{
+			return Task.CompletedTask;
+		}
+
+		[Fact(Skip = "not supported - reverse geocode")]
+		public override Task CanReverseGeocodeAsync()
+		{
+			return Task.CompletedTask;
+		}
+
+		[Theory(Skip = "not supported")]
+		public override Task CanGeocodeInvalidZipCodes(string address)
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
